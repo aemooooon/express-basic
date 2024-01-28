e1685b1f84492a17296dde4c8557c74ecbe1145e
Wang Hua <hua@Wang.local>
2024-01-29 00:24:10 +1300

    added env var

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const morgan = require("morgan");
 const moviesRouter = require("./routes/moviesRoutes");
 const app = express();
 
+app.use(express.json());
+app.use(express.static("./public")); // serving static files
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
 const logger = function (req, res, next) {
     console.log("custom middleware called");
     next();
 };
 
-app.use(express.json());
-app.use(morgan("combined"));
 app.use(logger);
 app.use((req, res, next) => {
     req.requestedAt = new Date().toISOString();
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// read config.env file with dotenv package
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
 // create a server
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("Server has started.");
+    console.log(`Server has started on PORT ${port}`);
 });
